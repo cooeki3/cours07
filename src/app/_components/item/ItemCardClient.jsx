de16203d8a26f3cc4b3861aa9d9fe50878f1eabf
EdouardCstj <2378255.cstj.qc.ca>
2025-10-06 12:26:30 -0400

    Animation item finit

--- src/app/_components/item/ItemCardClient.jsx
+++ src/app/_components/item/ItemCardClient.jsx
@@ -11,25 +11,30 @@ const ItemCardClient = () => {
   gsap.registerPlugin(SplitText);
 
   const titreItemRef = useRef();
-  useEffect(() => {
-    let containerTitre = document.getElementById("containerTitreItem");
-    containerTitre.style.overflow = "hidden";
+  // useEffect(() => {
+  //   let containerTitre = document.getElementById("containerTitreItem");
+  //   containerTitre.style.overflow = "hidden";
 
 
-  }, [])
+  // }, [])
+
+
+  const descrItemRef = useRef();
+  const imgItemRef = useRef();
 
 
   useGSAP(() => {
     const tl = gsap.timeline({
-      onComplete: () => {
-        let containerTitre = document.getElementById("containerTitreItem");
-        containerTitre.style.overflow = "visible";
-      }
+      // onComplete: () => {
+      //   let containerTitre = document.getElementById("containerTitreItem");
+      //   containerTitre.style.overflow = "visible";
+      // }
 
     });
+
     const splitTitre = SplitText.create("h2", { type: "words, chars" });
     const tlShake = gsap.timeline({ repeat: 20 });
-
+    const tlDepart = gsap.timeline();
     tlShake.fromTo(splitTitre.words,
       { x: 0, y: 0 },
       { x: "random(-40,40)", y: "random(-40,40)", duration: 0.01 }
@@ -53,10 +58,50 @@ const ItemCardClient = () => {
     tlShake.to(splitTitre.words,
       { x: 0, y: 0, duration: 0.01 }
     )
+    tl.to(titreItemRef.current,
+      {opacity:1,duration:0}
+    )
     tl.fromTo(splitTitre.chars,
-      { y: "100%" },
-      { y: 0, duration: 0.05, stagger: 0.25, ease: "power1.out" }
+      { y: "100%" ,opacity:0},
+      { y: 0,opacity:1, duration: 0.05, stagger: 0.25, ease: "power1.out" }
     ).add(tlShake)
+
+
+
+    tlDepart.to(descrItemRef.current,
+      { position: "absolute", duration: 0, opacity: 0 }
+    )
+    tlDepart.to(imgItemRef.current,
+      { position: "absolute", duration: 0, opacity: 0 },
+      "<"
+    )
+
+    tlDepart.to(titreItemRef.current,
+      { y: "20vh", duration: 0 ,opacity:1}
+    )
+
+    tlDepart.to(splitTitre.words,
+      { duration: 0 ,opacity:1},
+      "<"
+    )
+
+    tlDepart.fromTo(descrItemRef.current,
+      { opacity: 0, position: "absolute" },
+      { opacity: 1, position: "relative", duration: 1, delay: 0.5 }
+    )
+
+    tlDepart.fromTo(imgItemRef.current,
+      { opacity: 0, position: "absolute" },
+      { opacity: 1, position: "relative", duration: 1 }
+      , "<"
+    )
+
+    tlDepart.to(titreItemRef.current,
+      { x: 0, y: 0, duration: 0, ease: "power1.out",opacity:1 },
+      "<"
+    )
+    tlDepart.add(tl);
+
 
 
 
@@ -71,20 +116,20 @@ const ItemCardClient = () => {
 
   return (
     <div className="ItemCardClient">
-      <div id="containerTitreItem">
+      <div ref={titreItemRef} id="containerTitreItem">
         <h2
-          ref={titreItemRef}
+
         >Titre</h2>
       </div>
 
-      <div className="imgContainerItem">
+      <div ref={imgItemRef} className="imgContainerItem">
         <div className="divAbsoluteItem"></div>
         <img src="img/jpg/Sac_01.jpg" alt="Sac a dos" />
       </div>
 
 
 
-      <p>La légende raconte qu’il appartient à un influenceur qui a pour hobby de faire des hikes.</p>
+      <p ref={descrItemRef} >La légende raconte qu’il appartient à un influenceur qui a pour hobby de faire des hikes.</p>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
 import ClientPage from "./_components/ClientPage";
 import ItemCardClient from "./_components/item/ItemCardClient";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <ClientPage />
-    // <ItemCardClient/>
+    
+    <div className={styles.page}>
+      <ClientPage />
+      {/* <ItemCardClient /> */}
+    </div>
+
   );
 }
