446e589719f649c374a5e9d04f5e699d281a8bd0
EdouardCstj <2378255.cstj.qc.ca>
2025-10-06 09:32:46 -0400

    2Page plus fonts

---
+++ src/app/_components/item/ItemCardClient.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import "./ItemCardClient.css";
+
+
+const ItemCardClient = () => {
+  return (
+    <div className="ItemCardClient">
+      <h2>Titre</h2>
+      <img src="img/jpg/Sac_01.jpg" alt="Sac a dos" />
+      <p>La légende raconte qu’il appartient à un influenceur qui a pour hobby de faire des hikes.</p>
+    </div>
+  )
+}
+
+export default ItemCardClient
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 import ClientPage from "./_components/ClientPage";
+import ItemCardClient from "./_components/item/ItemCardClient";
 
 export default function Home() {
   return (
-    <ClientPage/>
+    // <ClientPage/>
+    <ItemCardClient/>
   );
 }
