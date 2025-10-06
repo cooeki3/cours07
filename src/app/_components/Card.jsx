864b179a07759baa19cd9b0adbdb8e4db1d311a4
Emilie <2073129@cstj.qc.ca>
2025-10-06 09:10:35 -0400

    css cards

--- src/app/_components/Card.jsx
+++ src/app/_components/Card.jsx
@@ -3,9 +3,9 @@ import "./Card.css";
 const Card = ({ titre, description, img }) => {
   return (
     <div className="card">
-      <img src={`/img/jpg/${img}`} alt={titre} />
-      <h3>{titre}</h3>
-      <p>{description}</p>
+      <img className="card-img" src={`/img/jpg/${img}`} alt={titre} />
+      <h3 className="card-titre">{titre}</h3>
+      <p className="card-description">{description}</p>
     </div>
   );
 };
