569123846a233dba45e2cf01b9ffef87862eb549
Alexandre <6233423@cstj.qc.ca>
2025-10-06 10:23:24 -0400

    Aniamtions + Header

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Halloween Store!</h1>
+      <p>Venez acheter nos sac à dos méticuleusement kidnapper</p>
+    </header>
+  );
+};
+export default Header;
